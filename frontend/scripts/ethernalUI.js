2223a20dfdc300a3a791f23a535e3e320755b783
Pablo Pfister <p.pfister@ticino.com>
2019-11-17 11:15:43 +0100

    Fix image

--- frontend/scripts/ethernalUI.js
+++ frontend/scripts/ethernalUI.js
@@ -1,13 +1,15 @@
 $('.dropdown-menu a').click(function(){
     var buff = $('#dropdownMenuButton').text()
 
+    console.log(buff, buff.toUpperCase(), buff.toUpperCase() === 'F**K YOU');
+
     $('#dropdownMenuButton').text($(this).text());
     $(this).text(buff)
 
     buff = $('#dropdownMenuButton').text();
     if(buff.toUpperCase() === 'F**K YOU'){
       $('.img-fluid').attr("src","img/middle-finger-cactus.svg");
-  }else if(buff.toUpperCase === 'LOVE YOU'){
+  }else if(buff.toUpperCase() === 'LOVE YOU'){
       $('.img-fluid').attr("src","img/undraw_intense_feeling_ft9s.svg")
     }
 
