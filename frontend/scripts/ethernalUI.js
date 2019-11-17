b897705c54aacc2f32cbb57012b13394cb99c97e
RyukNet <38398145+RyukNet@users.noreply.github.com>
2019-11-17 09:11:28 +0100

    Update ethernalUI.js

--- frontend/scripts/ethernalUI.js
+++ frontend/scripts/ethernalUI.js
@@ -1,5 +1,14 @@
 $('.dropdown-menu a').click(function(){
     var buff = $('#dropdownMenuButton').text()
+    
     $('#dropdownMenuButton').text($(this).text());
     $(this).text(buff)
-  });
+
+    buff = $('#dropdownMenuButton').text();
+    if(buff === 'FUCK YOU'){
+      $('.img-fluid').attr("src","img/middle-finger-cactus.svg");
+    }else if(buff === 'LOVE YOU'){
+      $('.img-fluid').attr("src","img/undraw_intense_feeling_ft9s.svg")
+    }
+
+});
